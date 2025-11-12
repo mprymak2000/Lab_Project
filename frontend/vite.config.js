d80c18f2e3bdcad0255aa3ec91b10f78bf88ae4d
Michal Prymak <mprymak2000@gmail.com>
2025-11-12 15:13:54 -0500

    m

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
     port: 3000,
-    host: '0.0.0.0',
+    host: 'localhost',
     watch: {
       usePolling: true,
     }
